7e8c813a8f75cda8a8cd63f40e3a4a37f085d0e6
Daniel Satnic <S01031109@acad.tri-c.edu>
2022-05-13 21:46:29 -0400

    got the menu working

--- src/GraphRunner.java
+++ src/GraphRunner.java
@@ -1,25 +1,33 @@
 import java.util.Scanner;
 
 public class GraphRunner {
+	
+	static Scanner input = new Scanner(System.in);
 
 	public static void main(String[] args) {
+		Integer menuEntry;
+
 		System.out.println("Graph has been loaded!");
 
 		displayMenu();
+		do {
+			menuEntry = parseUserInput();
+		} while (menuEntry == null);
 
+		input.close();
 	}
-	
+
 	private static void displayBasicGraphInfo() {
 		System.out.println("**********************");
 		System.out.println("X vertices");
 		System.out.println("Y edges");
 		System.out.println("**********************");
 	}
-	
+
 	private static void displayFullGraph() {
 		System.out.println("Full Graph");
 	}
-	
+
 	private static void displayMenu() {
 		System.out.println("What would you like to do?");
 		System.out.println("1) Display Basic Graph Info");
@@ -27,8 +35,18 @@ public class GraphRunner {
 		System.out.println("3) Breadth-First Search");
 		System.out.println("4) Depth-First Search");
 		System.out.println("5) Dijkstra's Algorithm Search");
-				
-		Scanner scan = new Scanner(System.in);
-        System.out.print("Please select an option: ");
 	}
+
+	private static Integer parseUserInput() {
+		
+		System.out.print("Please select an option: ");
+		int menuEntry = input.nextInt();
+		
+		if (menuEntry < 1 || menuEntry > 5) {
+			System.out.println("Invalid selection");
+			return null;
+		}
+		return menuEntry;
+	}
+
 }
