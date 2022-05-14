6b4dfd3a1373509222547939ce73c1ddb8946324
Daniel Satnic <S01031109@acad.tri-c.edu>
2022-05-13 21:48:57 -0400

    created an edge class

---
+++ src/GraphEdge.java
@@ -0,0 +1,21 @@
+public class GraphEdge {
+	private Integer connectedTo;
+	
+	private int weight;
+
+	public Integer getConnectedTo() {
+		return connectedTo;
+	}
+
+	public void setConnectedTo(Integer connectedTo) {
+		this.connectedTo = connectedTo;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+}
